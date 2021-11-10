934727e260296ffb75354987325a263943e3c810
sumeshir26 <sumeshir26@protonmail.com>
2021-11-10 10:33:16 +0530

    Add files

---
+++ configurator.py
@@ -0,0 +1,46 @@
+import tkinter
+from tkinter import ttk
+
+def createManagerWindow(saveTimer, current_mins, current_secs, current_hrs):
+    manager_app_window = tkinter.Tk()
+    manager_app_window.geometry('200x150')
+    manager_app_window.title('TimerX Timer Manager')
+    
+    # APP THEME
+    manager_app_window.tk.call("source", "sun-valley.tcl")
+    manager_app_window.tk.call("set_theme", "light")
+
+    # WINDOW FRAME
+    manager_window = ttk.Frame(manager_app_window)
+    manager_window.pack(fill="both", expand=True)
+
+    timer_hr_label = ttk.Label(manager_window, text = 'Hours: ')
+    timer_hr_label.grid(row = 1, column = 1, pady = 5)
+    timer_hr_input = tkinter.Entry(manager_window)
+    timer_hr_input.grid(row = 1, column = 2, pady = 5)
+    timer_hr_input.insert(1, current_hrs)
+
+    timer_min_label = ttk.Label(manager_window, text = 'Minutes: ')
+    timer_min_label.grid(row = 2, column = 1, pady = 5)
+    timer_min_input = tkinter.Entry(manager_window)
+    timer_min_input.grid(row = 2, column = 2, pady = 5)
+    timer_min_input.insert(1, current_mins)
+
+    timer_sec_label = ttk.Label(manager_window, text = 'Seconds: ')
+    timer_sec_label.grid(row = 3, column = 1, pady = 5)
+    timer_sec_input = tkinter.Entry(manager_window)
+    timer_sec_input.grid(row = 3, column = 2, pady = 5)
+    timer_sec_input.insert(1, current_secs)
+
+    ok_button = ttk.Button(manager_window, text = 'Ok!', command = lambda:saveTimer(timer_sec_input, timer_min_input, timer_hr_input, manager_window, manager_app_window))
+    ok_button.grid(row = 4, column = 2, pady = 5)
+
+def createAboutWindow():
+    about_window = tkinter.Tk()
+    about_window.geometry('300x200')
+    about_window.title('TimerX Timer Manager')
+
+    logo = tkinter.PhotoImage(file = 'assets/images/logo.jpeg')
+
+    about_window = tkinter.ttk(about_window, image = logo, bd = 0)
+    about_window.grid(pady = 20)
---
+++ main.py
@@ -0,0 +1,127 @@
+# TimerX v1.3.0 by sumeshir26
+# IMPORTS
+from time import sleep
+from tkinter import ttk, Tk, PhotoImage, Frame
+from playsound import playsound
+import threading
+import configurator
+
+# TKINTER WINDOW
+app = Tk()
+app.title('TimerX')
+app.geometry('300x200')
+
+# VARIABLES
+app_on = True
+
+default_font = 'assets/fonts/font.ttf'
+
+timer_on = False
+timer_paused = True
+
+timer_seconds = 5
+timer_minutes = 0
+timer_hours = 0
+
+# FUNCTIONS
+def playBuzzer():
+    playsound('assets/sounds/sound1.wav')
+
+def startstopButtonPressed():
+    global timer_on, timer_paused
+    if timer_on:
+        timer_on = False
+        timer_paused = True
+        # play_button.configure(image = play_button_img)
+        play_button.configure(text = "Play")
+    elif timer_paused == False and timer_on == False:
+        #play_button.configure(image = pause_button_img)
+        play_button.configure(text = "Pause")
+        timer_thread = threading.Thread(target=runTimer, daemon=True)
+        timer_thread.start()
+    else:
+        timer_paused = False
+
+def saveTimer(timer_sec_input, timer_min_input, timer_hr_input, manager_window, manager_app):
+    global timer_seconds, timer_minutes, timer_hours
+    try:
+        timer_seconds = int(timer_sec_input.get())
+    except ValueError:
+        time_selected_display.configure(text = "Please enter a number!")
+        timer_minutes = int(timer_min_input.get())
+        timer_hours = int(timer_hr_input.get())
+        time_selected_display.configure(text = f'Time Selected : {timer_seconds} Seconds')
+        time_display.configure(text = f'{timer_hours} : {timer_seconds} : {timer_seconds}')
+        print('DESTRUCTION MODE')
+        manager_app.destroy()
+    except ValueError:
+        time_selected_display.configure(text = "Please enter a number!")
+def runTimer():
+    global timer_seconds, timer_minutes, timer_hours, timer_on
+    seconds_left = timer_seconds
+    minutes_left = timer_minutes
+    hours_left = timer_hours
+    timer_done = False
+    timer_on = True
+    while timer_done == False:
+        if timer_on:
+            sleep(1)
+            #execution_start_time = time.time()
+            time_display.configure(text = f'{hours_left} : {minutes_left} : {seconds_left}')
+            if seconds_left == 0 and minutes_left != 0:
+                minutes_left -= 1
+                seconds_left = 59
+            elif seconds_left == 0 and minutes_left == 0 and hours_left != 0:
+                hours_left -= 1
+            elif seconds_left == 0 and timer_minutes == 0 and hours_left == 0:
+                timer_done = True
+            else:
+                seconds_left -= 1 #(time.time() - execution_start_time)
+        elif timer_paused == False:
+            seconds_left = timer_seconds
+            minutes_left = timer_minutes
+            hours_left = timer_hours
+            time_display.configure(text = f'{timer_hours} : {timer_minutes} : {timer_seconds}')
+        else:
+            time_display.configure(text = f'{hours_left} : {minutes_left} : {seconds_left}')
+
+    timer_on = False
+    time_display.configure(text = f'{hours_left} : {minutes_left} : {seconds_left}')
+    #play_button.config(image = play_button_img)
+    play_button.config(text = "Pause")
+    playBuzzer()
+    
+# IMAGES
+# logo_img = PhotoImage(file = 'assets/images/logo.jpeg')
+# app.iconphoto(False, logo_img)
+
+# play_button_img = PhotoImage(file = 'assets/images/play_arrow.png')
+# pause_button_img = PhotoImage(file = 'assets/images/pause_bars.png')
+
+# APP THEME
+app.tk.call("source", "sun-valley.tcl")
+app.tk.call("set_theme", "light")
+
+# WINDOW FRAME
+window = Frame(app)
+window.pack(fill="both", expand=True)
+
+# WINDOW ELEMENTS
+time_selected_display = ttk.Label(master = window, text = f'Time Selected : {timer_seconds} Seconds')
+time_selected_display.pack()
+
+time_display = ttk.Label(master = window, text = f'{timer_hours} : {timer_minutes} : {timer_seconds}', font = ("any", 30))
+time_display.pack(pady = 5)
+
+# play_button = ttk.Button(master = window, image = play_button_img, width = 20, command = startstopButtonPressed, style="Accent.TButton")
+play_button = ttk.Button(master = window, text = "Play", width = 25, command = startstopButtonPressed, style="Accent.TButton")
+play_button.pack()
+
+manager_button = ttk.Button(master =window, text = 'Edit Timer', command = lambda: configurator.createManagerWindow(saveTimer, timer_minutes, timer_seconds, timer_hours), width = 25)
+manager_button.pack(pady = 5)
+
+about_button = ttk.Button(master =window, text = 'About', command = configurator.createAboutWindow, width = 25)
+about_button.pack()
+
+# TKINTER MAINLOOP
+window.mainloop()
