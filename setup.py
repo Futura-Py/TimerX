ad39a871c2caa6e0b84e243438f1648038c11f10
sumeshir26 <sumeshir26@protonmail.com>
2021-12-08 15:38:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,23 @@ from cx_Freeze import setup, Executable
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
-    icon="assets/icon.ico"
+    icon="./assets/icon.ico"
 If sys.platform == "darwin":
-    Icon="assets/icon.icns"
+    Icon="./assets/icon.icns"
 
-executables = [Executable("main.py", base=base, icon=icon)]
+executables = [Executable("main.py", base=base, icon=icon, shortcutName="TimerX")]
+
+bdist_msi = {
+    add_to_path=False
+    install_icon="./assets/icon.ico"
+    upgrade_code="ADDLATER"
+    target_name="TimerX"
+}
 
 setup(
     name="TimerX",
-    version="0.1",
-    description="Timer application based on Python + rdbende's Sun Valley theme",
+    version="1.0-alpha",
+    description="A cross-platform siple and modern timer app",
     executables=executables,
+    bdist_msi=bdist_msi
 )
