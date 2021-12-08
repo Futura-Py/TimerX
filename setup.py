76a45ad379422a48366937c65b4c7454fa7ba2bc
sumeshir26 <sumeshir26@protonmail.com>
2021-12-08 20:03:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import sys
 from cx_Freeze import setup, Executable
 
@@ -6,10 +5,29 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
     icon="./assets/icon.ico"
-If sys.platform == "darwin":
-    Icon="./assets/icon.icns"
+elif sys.platform == "darwin":
+    icon="./assets/icon.icns"
+else;
+    icon= "./assets/icon.png"
 
-executables = [Executable("main.py", base=base, icon=icon, shortcutName="TimerX")]
+directory_table = [
+    ("ProgramMenuFolder", "TARGETDIR", "."),
+    ("MyProgramMenu", "ProgramMenuFolder", "MYPROG~1|My Program"),
+]
+
+msi_data = {
+    "Directory": directory_table,
+    "ProgId": [
+        ("TimerX", None, None, "A simple, lightweight, & beautiful timer app built in Python and tkinter.ttk using rdbende's Sun Valley TTk Theme", "IconId", None),
+    ],
+    "Icon": [
+        ("IconId", "assets/icon.ico"),
+    ],
+}
+
+executables = [Executable("main.py", base=base, icon=icon, shortcutName="TimerX", shortcutDir="TimerX")]
+
+build_exe_options = {"includes": ["tkinter, platform, threading"], "include_msvcr": True}
 
 bdist_msi = {
     add_to_path=False
