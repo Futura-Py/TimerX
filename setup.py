628c5934a3d8091013ed82d2f381339de6432c61
LG <76845820+im-coder-lg@users.noreply.github.com>
2021-12-08 14:19:21 +0530

    Fix `"`s

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from cx_Freeze import setup, Executable
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
-    icon=“assets/icon.ico”
-If sys.platform == “darwin”:
-    Icon=“assets/icon.icns”
+    icon="assets/icon.ico"
+If sys.platform == "darwin":
+    Icon="assets/icon.icns"
 
 executables = [Executable("main.py", base=base, icon=icon)]
 
