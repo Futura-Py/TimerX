7c8d66c5af6823ab4d7f4a3b6f40ec798cc0d257
sumeshir26 <sumeshir26@protonmail.com>
2021-12-10 14:09:46 +0530

    Actions fix (#3)
    * Update main.yml
    
    Test updated checks file
    
    * Update main.yml
    
    * Add Workflow Dispatch to run on command.
    
    * Create release-build.yml
    
    This is based on the Pyinstaller build action mentioned in sumeshir26#5, this doesn't have push/pull triggers, it has a workflow dispatch.
    
    * Update release-build.yml
    
    Try fixing YAML error
    
    * Update release-build.yml
    
    * Update release-build.yml
    
    * Rename release-build.yml to release.yml
    
    * Delete release.yml
    
    * Create release.yml
    
    * Update release.yml
    
    * Create setup.py
    
    * Modify Setup Data
    
    * Fix `"`s
    
    * feat: Actions for Mac and Ubuntu (#2)
    
    * Update release.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Deactivate blur.
    
    We could make it an optional feature, that'd be good, people can just remove the double-apostrophes(""") to uncomment the blur.
    
    * Update requirements.txt
    
    * Create optional-requirements.txt
    
    People can enable blur if they want. We shall add a disclaimer on the README saying the app is in beta and the Mica Acrylic blur is buggy.
    
    * Create packager-requirements.txt
    
    * feat: Add setup for macos!
    
    * Rename setup.py
    
    * fix: Fix pyinstaller in requirements.txt
    
    * Remove blur(again)
    
    * Delete macos-setup.py
    
    Co-authored-by: sumeshir26 <sumeshir26@protonmail.com>
    
    * Remove macOS and Linux tests _temporarily_
    
    * Rename `name` to `version` in `workspace_dispatch`
    
    * Add cx_Freeze to packager requirements
    
    * Update setup.py
    
    * Update setup.py
    
    * fix: WORKING MSI!!!
    
    * testmode: Test `bdist_rpm`
    
    * Remove RPM for now.
    
    * fix: Fix UID
    
    * Update setup.py
    
    * Update release.yml
    
    * Update release.yml
    
    * Rename Asset path
    
    * Update release.yml
    
    * Update release.yml
    
    * Rename release.yml to release-windows.yml
    
    * Create release-mac.yml
    
    * Update release-mac.yml
    
    * Update release-mac.yml
    
    * Update release-windows.yml
    
    * Rename job name
    
    * try fixing workflow dispatch error
    
    * Delete release-windows.yml
    
    * Delete release-mac.yml
    
    Co-authored-by: LG <76845820+im-coder-lg@users.noreply.github.com>
    Co-authored-by: LG <imcoderlg@protonmail.com>

---
+++ setup.py
@@ -0,0 +1,65 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+    icon="./assets/logo.ico"
+elif sys.platform == "darwin":
+    icon="./assets/logo.icns"
+else:
+    icon= "./assets/logo.png"
+
+# directory_table = [
+#     ("ProgramMenuFolder", "TARGETDIR", "."),
+#     ("MyProgramMenu", "ProgramMenuFolder", "MYPROG~1|My Program"),
+# ]
+
+# msi_data = {
+#     "Directory": directory_table,
+#     "ProgId": [
+#         ("TimerX", None, None, "A simple, lightweight, & beautiful timer app built in Python and tkinter.ttk using rdbende's Sun Valley TTk Theme", "IconId", None),
+#     ],
+#     "Icon": [
+#         ("IconId", "assets/icon.ico"),
+#     ],
+# }
+import uuid
+upgradeid = str(uuid.uuid3(uuid.NAMESPACE_DNS, 'TimerX.TimerX.org')).upper()
+# print(str(uuid.uuid3(uuid.NAMESPACE_DNS, 'TimerX.TimerX.org')).upper())
+
+executables = [Executable("main.py", base=base, icon=icon, shortcut_name="TimerX", shortcut_dir="TimerX")]
+
+# build_exe_options = {"includes": ["tkinter, platform, threading"], "include_msvcr": True}
+build_exe_options = {
+    "include_msvcr": True,
+    "include_files":("sun-valley.tcl", "./theme", "./assets", "configurator.py"),
+}
+
+bdist_msi_options = {
+    "add_to_path":False,
+    "install_icon":"assets/logo.ico",
+    #"upgrade_code":upgradeid,
+    "target_name":"TimerX"
+}
+bdist_mac_options = {
+    "bundle_name": "TimerX",
+    "iconfile":"./assets/logo.icns"
+}
+
+bdist_dmg_options = {
+    "volume_label": "TimerX",
+}
+
+setup(
+    name="TimerX",
+    version="1.0.0.0",
+    description="A cross-platform simple and modern timer app",
+    executables=executables,
+    options={
+        "build_exe": build_exe_options,
+        "bdist_mac": bdist_mac_options,
+        "bdist_dmg": bdist_dmg_options,
+        "bdist_msi": bdist_msi_options,
+    },
+)
