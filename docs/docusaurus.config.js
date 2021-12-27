96a7fafec03c459e4e75fe018041cd53ab8e1591
LG <76845820+im-coder-lg@users.noreply.github.com>
2021-12-27 16:17:40 +0530

    Remove the Community part
    It's not as if we are contributing from inside an organization.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -68,23 +68,6 @@ const config = {
             ],
           },
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
