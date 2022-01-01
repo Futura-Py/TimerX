48eb14e26e14324c3151a877a937c1137817e4e2
LG <76845820+im-coder-lg@users.noreply.github.com>
2022-01-01 11:26:26 +0530

    feat: Website with Docusaurus (#26)
    * Update Gitpod config and add noVNC support.
    
    * Add the entire website code(JTD, CODEDOC)
    
    * Gemfile...
    
    * Add Gitignores
    
    * save progress, delete codedoc*.* soon.
    
    * Create Vercel config for Vercel test deployment
    
    * Remove Jekyll and CODEDOC(im sorry)
    
    * oops
    
    * Backup adding Docusaurus(its cool!)
    testing
    
    * Add website init code, configure Python server
    Check the comment too
    
    * Configure URLs ticked off
    
    * Remove blog(and hopefully 8000 additions)
    
    * replace icons ticked off
    
    * Back up progress.
    Till now, favcion replaced, links changed, documentation removed.
    Blog removed too.
    
    * Finally, disable the blog!
    
    * Testing MDX while retaining the JS file
    
    * hopefully disable build errors(read description for more info)
    This might be a problem but since I removed the blog, it throws errors.
    Docusaurus syntax says that we can choose to ignore, but I thought `warn` would be better.
    
    * Comment out react module
    since it seems to exist on MDX
    
    * Try fixing slug issue
    
    * Jackpot!!!
    we did the redesign!
    
    * correct my grammar
    
    * Remove the Community part
    
    It's not as if we are contributing from inside an organization.
    
    * feat: Dark Mode icons
    I will try following the pro way of naming commits.
    
    * feat: Add announcement bar
    That would ask more people to contribute and help us.
    
    * Change image, add NVM-SH tasks
    Seems unrelevant but the Gitpod image...
    That is to blame.
    
    * Add branch
    Stupid me

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,10 @@
+# https://hub.docker.com/r/gitpod/workspace-python-tk-vnc # Python with Tk and VNC
+FROM gitpod/workspace-python-tk-vnc:branch-tk-dev
+
+RUN echo "Installing Node.js for Docusaurus"
+RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+RUN export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+RUN source ~/.bashrc
+RUN echo "NVM installed."
+RUN nvm install 14
+RUN nvm use 14
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,124 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'TimerX',
+  tagline: "A simple, lightweight, & beautiful timer app built in Python and tkinter.ttk using rdbende's Sun Valley TTk Theme",
+  url: 'https://timerx.vercel.app',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'sumeshir26', // Usually your GitHub org/user name.
+  projectName: 'TimerX', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/sumeshir26/TimerX',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  customFields: {
+    throwIfNamespace: false,
+  },
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'TimerX',
+        logo: {
+          alt: 'TimerX Logo',
+          src: 'img/logo.svg',
+        },
+        
+        items: [
+          {to: '/docs/intro', label: 'Documentation', position: 'left'},
+          {to: 'https://github.com/sumeshir26/TimerX/releases', label: "Download TimerX", position: 'left'},
+          {
+            href: 'https://github.com/sumeshir26/TimerX',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Under work',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/sumeshir26/TimerX',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} MIT License by sumeshir26`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+        switchConfig: {
+          darkIcon: '\u263e',
+          darkIconStyle: {
+            marginLeft: '2px',
+          },
+          /*
+          Unicode icons such as '\u2600' will work
+          Unicode with 5 chars require brackets: '\u{1F602}'
+          but I don't get the point of unicodes when we have GitHub emojis, right?
+          // this is old. lightIcon: '\u{1F602}',
+          */
+          lightIcon: '\u{1F4A1}',
+          lightIconStyle: {
+            marginLeft: '1px',
+          },
+        },
+      },
+      announcementBar: {
+        id: 'under_development',
+        content:
+          'TimerX is under heavy development. If you would like to contribute, please visit the <a href="https://github.com/sumeshir26/TimerX">GitHub repo</a>.',
+        backgroundColor: '#fafbfc',
+        textColor: '#091E42',
+        isCloseable: true,
+      },
+    }),
+};
+
+module.exports = config;
