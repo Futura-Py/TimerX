cc4213fbef7f9352a6e9db7c067e54defa95f34b
sumeshir26 <68823982+sumeshir26@users.noreply.github.com>
2022-01-02 13:56:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ def runTimer():
             notification = ToastNotifier()
             notification.show_toast(
             "TimerX", 
-            "Timer done!", 
+            "Time's up!", 
             icon_path='./assets/logo.ico', 
             duration='None', 
             threaded=True,
