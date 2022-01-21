0545f0a96d6d6c14c9a4031fd321bb855ea5a1c5
LG <76845820+im-coder-lg@users.noreply.github.com>
2022-01-21 13:54:29 +0530

    docs: Update link redirect
    Should have fixed this before... Oh well.

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://github.com/sumeshir26/TimerX/releases"
+            to="https://github.com/TimerX-App/TimerX/releases/latest"
           >
             Download
           </Link>
